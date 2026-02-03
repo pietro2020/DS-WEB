a87669833b00595058bf8a7fddc1e6b5e1d59390
pietro2020 <pietrovonovono@gmail.com>
2026-02-03 14:08:03 -0300

    Primeiro commit de 2026

---
+++ PBE/POO/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h1>Aulas de Orientação a Objeto!</h1>";
+?>
---
+++ PBE/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Principal</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Aulas</h1>
+    <main>
+        <a href="/POO/">Aula de POO</a>
+    </main>
+</body>
+</html>
